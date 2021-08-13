5095c9e41ac7a21c28b91d87557bab96c296e7e8
Bobby <monkeyeden.rjt@gmail.com>
2021-08-13 13:08:05 -0400

    DB created

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+require('dotenv').config()
+
+const express = require('express');
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(helmet());
+server.use(express.json());
+
+module.exports = server
