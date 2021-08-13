bf4b705a8611554c48f925d507078a1d2420d10c
Bobby <monkeyeden.rjt@gmail.com>
2021-08-13 14:37:23 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -9,4 +9,19 @@ const server = express()
 server.use(helmet());
 server.use(express.json());
 
-module.exports = server
+server.get('/', (req, res, next) => {
+    res.send(`
+      <h2>This is a test</h2>
+      <p>Testing</p>
+    `);
+  });
+  server.use('*', (req, res, next) => {
+    console.log(`hitting ${req.method} ${req.baseUrl}`);
+    next({ status: 404, message: 'not found' }); // this object becomes the "err" in the midd below
+  });
+  
+  server.use((err, req, res, next) => { 
+    res.status(err.status || 500).json({ message: `Not working` });
+  });
+
+module.exports = server;
